a58733e1915f67e6c453da3318fd567d91c5bc48
Balogzee <seunzi2122@gmail.com>
2021-04-06 05:39:04 +0100

    Finished Dict, Cond & Loops

--- Dictionaries_Conditionals&Loops.py
+++ Dictionaries_Conditionals&Loops.py
@@ -18,22 +18,22 @@ fruit_basket = {
 #print(len(first_dict))
 #print(len(fruit_basket))
 
-mangoes = fruit_basket["mango"]
-print(f"We have {mangoes} mangoes in the basket")
+# mangoes = fruit_basket["mango"]
+# print(f"We have {mangoes} mangoes in the basket")
 
-apples = fruit_basket.get("berrys", 0)
-print(f"We have {apples} berry's in the basket")
+# apples = fruit_basket.get("berrys", 0)
+# print(f"We have {apples} berry's in the basket")
 
-#gets all the keys
-all_fruit_keys = fruit_basket.keys()
-print(all_fruit_keys)
+# #gets all the keys
+# all_fruit_keys = fruit_basket.keys()
+# print(all_fruit_keys)
 
 #add new keys and values
-fruit_basket.update({"berrys":24})
-print(fruit_basket.items())
+# fruit_basket.update({"berrys":24})
+# print(fruit_basket.items())
 
-#remove a key and its value, while popitem() removes the last item in the dictionary
-fruit_basket.pop("berrys")
+# #remove a key and its value, while popitem() removes the last item in the dictionary
+# fruit_basket.pop("berrys")
 #or 
 #del fruit_basket("berrys")
 """
@@ -41,9 +41,9 @@ But be careful 'del' doesn't just delete the values in a list, dict, var, etc. i
 """
 
 #To clear basket
-fruit_basket.clear()
+# fruit_basket.clear()
 
-print(fruit_basket)
+# print(fruit_basket)
 
 #To copy a dictionary
 new_fruit_basket = fruit_basket #This does not copy it only references
@@ -51,5 +51,19 @@ new_fruit_basket = fruit_basket #This does not copy it only references
 #Shallow copy uses the reference of the copiee, so when the original changes, the copy also changes
 #Deep Copy creates its own reference, the recursively insert copies from the object found in the original
 
-new_fruit_basket1 = fruit_basket.copy() #Still shallow copy
-new_fruit_basket2= copy.deepcopy(fruit_basket) #deep copy
+# new_fruit_basket1 = fruit_basket.copy() #Still shallow copy
+# new_fruit_basket2 = copy.deepcopy(fruit_basket) #deep copy
+
+
+item_i = ["beans", "rice", "yam"]
+item_j = ["stew", "egg", "vegetable"]
+
+for index, item2 in enumerate(item_i):
+    print(f"{index} and {item2}")
+
+for i in item_i:
+    for j in item_j:
+        print(i, " and ", j)
+
+for key, value in fruit_basket.items():
+    print(key, " = ", value)
