8efc233bdaa5e1e2f42f93130a87b468d96b530b
Balogzee <seunzi2122@gmail.com>
2021-04-07 09:38:12 +0100

    Completed code, starting debugger

--- ATM_Mock_Project/ATM_mock_project.py
+++ ATM_Mock_Project/ATM_mock_project.py
@@ -1,78 +1,87 @@
 import time
+import Authentication as auth
+import pickle
 
 localtime = time.asctime(time.localtime(time.time()))
-userNames = ["Seun", "Joy", "Ife"]
-userPassword = ["Seun", "Joy", "Ife"]
-userBalance = [20000, 30000, 50000]
-complaint = []
+userBalance = []
+loaded_balance = pickle.load(open("Balance.txt", "rb"))
+userBalance = loaded_balance
+complaint = {}
+loaded_complaint = pickle.load(open("Complaints.txt", "rb"))
 
 def atm():
     print("****************************************")
     print(localtime)
     print("****************************************")
 
-    print("Hello there!".title())
-    name = input("Please Enter Your Name: \n".title())
-    if(name in userNames):
-        password = input("Enter Password: \n".title())
-        if(password == userPassword[userNames.index(name)]):
-            print("****************************************")
-            print("Welcome, " + str(name))
-            print("Please Select An Option".title())
-            print("1. Withdrawal")
-            print("2. Deposit")
-            print("3. Complaint:")
-            
-            selectedOption = int(input(":"))
-
-            if(selectedOption == 1): #Withdrawal
-                print("****************************************")
-                print("Withdarwal")
-                print("How much would you like to withdraw?:".title())
-                withdrawalAmount = int(input(":"))
-                userBalance[userNames.index(name)] -= withdrawalAmount
-                print("****************************************")
-                print("Please Take your cash".title())
-                print("Balance: " + str(userBalance[userNames.index(name)]))
-                
-            elif(selectedOption == 2): #Deposit
-                print("****************************************")
-                print("Deposit")
-                print("How much would you like to deposit:".title())
-                depositAmount = int(input(":"))
-                userBalance[userNames.index(name)] += depositAmount
-                print("****************************************")
-                print("Deposit completed".title())
-                print("Balance: " + str(userBalance[userNames.index(name)]))
-
-            elif(selectedOption == 3): #Complaint
-                print("****************************************")
-                print("Complaint")
-                print("What issue will you like to report?:".title())
-                complaint.append(input(":"))
-                print("****************************************")
-                print("Thank you for contacting us".title())
-
-            else:
-                print("Invalid Option Selected, please try again".title())
-
-        else:
-            print("Password Incorrect, please try again".title())
-
-    else:
-        print("No name found, please try again".title())
+    auth.init()
+    transactions()
 
     print("****************************************")
     print("Do you want to process another transaction? Y/N".title())
     response = input(":")
     if(response == "Y" or response == "Yes" or response == "y"):
-        atm()
+        transactions()
     else:
         print("****************************************")
         print("Thank you for banking with us".title())
         print("****************************************")
+        pickle.dump(auth.database, open("Database.txt", "wb"))
+        pickle.dump(userBalance, open("Complaints.txt", "wb"))
+        pickle.dump(complaint, open("Balance.txt", "wb"))
+        exit()
         pass
 
+def transactions():
+    print("Please Select An Option:")
+    print("1. Deposit")
+    print("2. Withdrawal")
+    print("3. Complaint")
+    print("4. Logout")
+    print("5. Exit: \n")
+    selectedOption = int(input())
+
+    if(selectedOption == 1):
+        print("****************************************")
+        print("Deposit")
+        print("How much would you like to deposit:".title())
+        depositAmount = int(input(":"))
+        userBalance[auth.database.index(auth.accountNumber)] += depositAmount
+        print("****************************************")
+        print("Deposit completed".title())
+        print("Balance: " + str(userBalance[auth.database.index(auth.accountNumber)]))
+        
+    elif(selectedOption == 2):
+        print("****************************************")
+        print("Withdarwal")
+        print("How much would you like to withdraw?:".title())
+        withdrawalAmount = int(input(":"))
+        userBalance[auth.database.index(auth.accountNumber)] -= withdrawalAmount
+        print("****************************************")
+        print("Please Take your cash".title())
+        print("Balance: " + str(userBalance[auth.database.index(auth.accountNumber)]))
+        
+    elif(selectedOption == 3):
+        print("****************************************")
+        print("Complaint")
+        print("What issue will you like to report?:".title())
+        complaint.update(auth.accountNumber, input(":"))
+        print("****************************************")
+        print("Complaint Issued\nThank you for contacting us".title())
+        
+    elif(selectedOption == 4):
+        auth.logout()
+        
+    elif(selectedOption == 5):
+        pickle.dump(auth.database, open("Database.txt", "wb"))
+        pickle.dump(userBalance, open("Complaints.txt", "wb"))
+        pickle.dump(complaint, open("Balance.txt", "wb"))
+        exit()
+    else:
+      
+        print("Invalid option selected, please try again".title())
+
+    
 atm()
 
 
--- ATM_Mock_Project/Authentication.py
+++ ATM_Mock_Project/Authentication.py
@@ -11,12 +11,15 @@
 
 #Initializing the system
 import random
+import pickle
 
 database = {} #dictionary
+loaded_data = pickle.load(open("Database.txt", "rb"))
+database = loaded_data
+
+accountNumber = 0
 
 def init():
-
-   
     print("Welcome to bankPHP")
  
     haveAccount = int(input("Do you have account with us: 1 (yes) 2 (no) \n"))
@@ -74,33 +77,9 @@ def register():
 def bankOperation(user):
 
     print("Welcome %s %s " % ( user[0], user[1] ) )
+    return 
 
-    selectedOption = int(input("What would you like to do? (1) deposit (2) withdrawal (3) Logout (4) Exit \n"))
-
-    if(selectedOption == 1):
-        
-        depositOperation()
-    elif(selectedOption == 2):
-        
-        withdrawalOperation()
-    elif(selectedOption == 3):
-        
-        logout()
-    elif(selectedOption == 4):
-        
-        exit()
-    else:
-      
-        print("Invalid option selected")
-        bankOperation(user)
-
-
-def withdrawalOperation():
-    print("withdrawal")
-
-
-def depositOperation():
-    print("Deposit Operations")
+    
 
 
 def generationAccountNumber():
